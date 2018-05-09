eee9fc3482118f4e47c4fc868bf4263e44a2836d
Antonio <antonio241710@gmail.com>
2018-05-09 22:28:02 +0200

    added CRUD list

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.products.index')->with(compact('products')); //listado
+    }
+
+    public function create()
+    {
+        return view('admin.products.create'); //formulario de registro
+    }
+
+    public function store()
+    {
+        //registrar nuevo producto en la bd
+    }
+}
