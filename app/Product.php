1d08db3ae7994305b7a15e0e631b3c35d2f62d6c
Antonio <antonio241710@gmail.com>
2018-05-17 19:58:15 +0200

    added accessor featured image

--- app/Product.php
+++ app/Product.php
@@ -18,4 +18,19 @@ class Product extends Model
         return $this->hasMany(ProductImage::class);
     }
 
+    public function getFeaturedImageUrlAttribute()
+    {
+        $featuredImage = $this->images()->where('featured', true)->first();
+
+        if (!$featuredImage){
+          $featuredImage = $this->images()->first();
+        }
+
+        if ($featuredImage){
+            return $featuredImage->url;
+        }
+
+        //devolver una imagen por defecto
+        return '/images/products/default.jpg';
+    }
 }
