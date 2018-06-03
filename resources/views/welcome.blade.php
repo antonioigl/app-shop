c5bf08d345f62d701076e25effd52c41daac7b8b
Antonio <antonio241710@gmail.com>
2018-06-03 11:48:20 +0200

    show categories home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,29 +84,21 @@
             </div>
 
             <div class="section text-center">
-                <h2 class="title">Productos disponibles</h2>
+                <h2 class="title">Visita nuestras categorías</h2>
 
                 <div class="team">
                     <div class="row">
-                        @foreach($products as $product)
+                        @foreach($categories as $category)
                             <div class="col-md-4">
                             <div class="team-player">
-                                <img src="{{$product->featured_image_url}}" alt="Thumbnail Image" class="img-raised img-circle">
+                                <img src="{{$category->featured_image_url}}" alt="Imagen representativa de la categoría {{$category->name}}" class="img-raised img-circle">
                                 <h4 class="title">
-                                    <a href="{{url('products/'.$product->id)}}">{{$product->name}}</a>
-                                    <br>
-                                    <small class="text-muted">{{$product->category_name}}</small>
+                                    <a href="{{url('categories/'.$category->id)}}">{{$category->name}}</a>
                                 </h4>
-                                <p class="description">{{$product->description}}</p>
-                                {{--<a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>--}}
-                                {{--<a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-instagram"></i></a>--}}
-                                {{--<a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i class="fa fa-facebook-square"></i></a>--}}
+                                <p class="description">{{$category->description}}</p>
                             </div>
                         </div>
                         @endforeach
-                    </div>
-                    <div class="text-center">
-                        {{$products->links()}}
                     </div>
                 </div>
 
