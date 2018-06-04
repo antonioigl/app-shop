096f49719e6b56e649d9445fa63ff2185437e06b
Antonio <antonio241710@gmail.com>
2018-06-05 00:08:56 +0200

    dynamic redirect after login

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -7,9 +7,13 @@ use Illuminate\Http\Request;
 
 class CartDetailController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Request $request)
     {
-
         $cartDetail = new CartDetail();
         $cartDetail->cart_id = auth()->user()->cart->id;
         $cartDetail->product_id = $request->product_id;
