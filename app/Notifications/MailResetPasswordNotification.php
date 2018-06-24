3df2539a25cd64989eebd4cd2d4a5008bf64ff1a
Antonio <antonio241710@gmail.com>
2018-06-24 19:47:51 +0200

    customizing template reset password

---
+++ app/Notifications/MailResetPasswordNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class MailResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $link = url( "/password/reset/token=" . $this->token );
+
+        return (new MailMessage)
+            ->subject('Restablecer Contraseña')
+            ->greeting('Has recibido este correo electrónico porque hemos recibido una solicitud de restablecimiento de contraseña para tu cuenta.')
+            ->line('Haga clic en el botón para restablecer tu contraseña')
+            ->action('Restablecer Contraseña', $link)
+            ->line('¡Gracias por utilizar AppShop!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
